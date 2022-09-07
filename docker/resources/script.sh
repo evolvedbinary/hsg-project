da65ac18938474a2e615ef7e9462e1bd96a0d7e3
marmoure <tinky93winky@gmail.com>
2022-09-07 12:51:24 +0200

    remove entrypoint

--- docker/resources/script.sh
+++ docker/resources/script.sh
@@ -2,19 +2,19 @@
 set -x
 
 #fetch args and declare them as vars
-ACTION=$1
+ACTION="${1:-build}"
 # using hsg-shell url as default repo if no fork was provided
 REPO="${2:-https://github.com/HistoryAtState/hsg-project.git}"
 BRANCH="${3:-master}"
 
 
-git clone $REPO
-cd hsg-project
-git checkout $BRANCH
-
-if [ $ACTION -eq "build"] then
+if [[ $ACTION == "build" ]]; then
     echo "building hsg-project from $REPO and branch $Branch"
-elif [ $ACTION -eq "build-one"] then
-    XAR=$4
-    echo "building one-xar $XAR from $REPO and branch $Branch"
+
+    git clone $REPO
+    cd hsg-project
+    git checkout $BRANCH
+    
+elif [[ $ACTION == "build-one" ]]; then
+    echo "building one-xar from $REPO and branch $Branch"
 fi
