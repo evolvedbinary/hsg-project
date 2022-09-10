359270fd4daaacad86948292c2a70e11920a2ebd
marmoure <tinky93winky@gmail.com>
2022-09-10 13:10:12 +0200

    test the deploying again

--- docker/resources/script.sh
+++ docker/resources/script.sh
@@ -8,10 +8,11 @@ if [[ $ACTION == "build" ]]; then
     echo "building hsg-project"
     sudo chmod -R a+rw /home/docker/hsg-project \
     && cd /home/docker/hsg-project \
-    && ant setup | tee setup.log \
+    && ant setup 2>&1 | tee setup.log \
     && rm repos/hsg-shell/.npmrc \
-    && sed -i -e 's/\^3.3.6/3.3.6/g' repos/hsg-shell/bower.json 
-    
+    && sed -i -e 's/\^3.3.6/3.3.6/g' repos/hsg-shell/bower.json \
+    && ant 2>&1 | tee ant.log
+
 elif [[ $ACTION == "build-one" ]]; then
     echo "building one-xar"
 fi
