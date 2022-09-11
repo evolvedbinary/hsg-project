946e5c8c7b94875a9fc1365412dccaf60c1ae478
marmoure <tinky93winky@gmail.com>
2022-09-11 20:51:04 +0200

    add sleep to ensure exist start up

--- docker/resources/script.sh
+++ docker/resources/script.sh
@@ -8,15 +8,16 @@ if [[ $ACTION == "build" ]]; then
     echo "building hsg-project"
     sudo chmod -R a+rw /home/docker/hsg-project \
     && cd /home/docker/hsg-project \
-    && ant setup 2>&1 | tee setup.log \
+    && ant clean \
+    && ant setup | tee setup.log \
     && rm repos/hsg-shell/.npmrc \
     && sed -i -e 's/\^3.3.6/3.3.6/g' repos/hsg-shell/bower.json \
     && curl --output exist.tar.bz2 --location https://github.com/eXist-db/exist/releases/download/eXist-6.0.1/exist-distribution-6.0.1-unix.tar.bz2 \
-    && bzip2 -d exist.tar.bz2 \
-    && tar xvf exist.tar \
+    && bzip2 -d exist.tar.bz2 && tar xvf exist.tar 
     ./exist-distribution-6.0.1/bin/startup.sh &
-    ant 2>&1 | tee ant.log
-    ./exist-distribution-6.0.1/bin/shutdown.sh
+    sleep 30 
+    ant | tee ant.log
+    ./exist-distribution-6.0.1/bin/shutdown.sh 
 
 elif [[ $ACTION == "build-one" ]]; then
     echo "building one-xar"
